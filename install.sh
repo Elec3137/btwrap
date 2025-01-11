11e8f79131edbc0af603e0a7356f73386b8d3d6a
Elec3137 <elec3137@proton.me>
2025-01-10 22:59:17 -0800

    sensical conditional in install script

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ while getopts s flag; do
 done
 sudo cp btwrap /bin/
 
-if [ "$systemd" ]; then
+if [ ! "$systemd" = true ]; then
     sudo cp btwrap.timer /etc/systemd/system/
     sudo cp btwrap.service /etc/systemd/system/
     sudo systemctl daemon-reload
